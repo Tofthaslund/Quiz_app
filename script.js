738478964984a311b2ab2b15662f0965f33542ef
Lasse Haslund <tofthaslund@gmail.com>
2021-10-17 14:53:31 +0100

    added 5 questions to JS file for use later

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'What is the Capital of Denmark',
+        a: 'Copenhagen',
+        b: 'Hobro',
+        c: 'Aarhus',
+        d: 'Aalborg',
+        correct: 'a'
+    },
+    {
+        question: 'What is the capital of Australia',
+        a: 'Sydney',
+        b: 'Melbourne',
+        c: 'Canberra',
+        d: 'Darwin',
+        correct: 'c'
+    },
+    {
+        question: 'What is the capital of USA',
+        a: 'New York',
+        b: 'Washington',
+        c: 'San Diego',
+        d: 'Los Angeles',
+        correct: 'b'
+    },
+    {
+        question: 'What is the capital of Vietnam',
+        a: 'Ho Chi Minh City',
+        b: 'Hanoi',
+        c: 'Da Nang',
+        d: 'Haiphong',
+        correct: 'b'
+    },
+    {
+        question: 'What is the capital of Brazil',
+        a: 'São Paulo',
+        b: 'Rio de Janeiro',
+        c: 'Brasillia',
+        d: 'Salvador',
+        correct: 'c'
+    },
+]
