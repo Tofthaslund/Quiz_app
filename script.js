dc9fdc011c4a387c81aa80e57e9c2d0d9b3930f5
Lasse Haslund <tofthaslund@gmail.com>
2021-10-17 21:02:17 +0100

    moved script tag to bottom of index.html to fix bug

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
-
+const submitBtn = document.getElementById("submit")
 
 
 let currentQuiz = 0;
@@ -59,11 +59,16 @@ function loadQuiz(){
 
 const currentQuizData = quizData[currentQuiz];
 
-    questionEl.innerText = currentQuizData.question;
+    questionEl.innerHTML = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
 
-    currentQuestion++;
 };
+
+submitBtn.addEventListener('click', () => {
+    currentQuiz++;
+
+    loadQuiz();
+});
